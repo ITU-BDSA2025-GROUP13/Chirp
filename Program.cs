514ce5649172c0ba29584944354aa4d00686ade7
Matthias Nielsen <matthias.s.nielsen@gmail.com>
2025-08-27 16:25:50 +0200

    Week1: Reading CSV file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text.RegularExpressions;
+
+
+if (args[0] == "read")
+{
+    var input = File.ReadAllLines("assets/chirp_cli_db.csv");
+
+    for (int i = 1; i < input.Length; i++)
+    {
+        var line = Regex.Split(input[i], ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");W
+    
+        string userMessage = line[1].Replace("\"", "");
+        DateTime dateFormat = DateTime.UnixEpoch.AddSeconds(long.Parse(line[2]) + 7200);
+        string dateString = dateFormat.Month.ToString("D2") + "/" + dateFormat.Day.ToString("D2") + "/" +
+                           dateFormat.Year.ToString().Substring(2, 2) + " " + dateFormat.ToString("HH:mm:ss");
+    
+        Console.WriteLine(line[0] + " @ " + dateString + ": " + userMessage);
+    }    
+}
