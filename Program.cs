4e4e113a9b7a8d1cf968c5d9491fa97744bdc654
natthias@proton.me <natthias@proton.me>
2025-08-27 16:31:45 +0200

    add saving cheeps

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿
 using System.Text.RegularExpressions;
 
+const string CSV = "assets/chirp_cli_db.csv";
 
 if (args[0] == "read")
 {
-    var input = File.ReadAllLines("assets/chirp_cli_db.csv");
+    var input = File.ReadAllLines(CSV);
 
     for (int i = 1; i < input.Length; i++)
     {
@@ -17,4 +18,15 @@ if (args[0] == "read")
     
         Console.WriteLine(line[0] + " @ " + dateString + ": " + userMessage);
     }    
+}
+else if (args[0] == "cheep")
+{
+	// Get metadata and cheep
+	string user = Environment.UserName;
+	long epoch = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+	string message = args[1];
+
+	// Format and save
+	var data = user + ',' + '"' + message + '"' + ',' + epoch + '\n';
+	File.AppendAllText(CSV, data);
 }
