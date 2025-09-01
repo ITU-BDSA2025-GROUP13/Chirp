f2ed7b5d830e90c82362308fa91c43cfc13412bf
Matthias Nyman <natthias@proton.me>
2025-09-01 22:11:13 +0200

    split off read and write functions from main

--- Program.cs
+++ Program.cs
@@ -8,31 +8,50 @@ class Program
     {
         if (args[0] == "read")
         {
-            var input = File.ReadAllLines(CSV);
-
-            for (int i = 1; i < input.Length; i++)
-            {
-                var line = Regex.Split(input[i], ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-
-                string userMessage = line[1].Replace("\"", "");
-                DateTime dateFormat = DateTime.UnixEpoch.AddSeconds(long.Parse(line[2]) + 7200);
-                string dateString = dateFormat.Month.ToString("D2") + "/" + dateFormat.Day.ToString("D2") + "/" +
-                                   dateFormat.Year.ToString().Substring(2, 2) + " " + dateFormat.ToString("HH:mm:ss");
-
-                Console.WriteLine(line[0] + " @ " + dateString + ": " + userMessage);
-            }
+            read();
         }
         else if (args[0] == "cheep")
         {
-            // Get metadata and cheep
-            string user = Environment.UserName;
-            long epoch = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             string message = args[1];
-
-            // Format and save
-            var data = user + ',' + '"' + message + '"' + ',' + epoch + '\n';
-            File.AppendAllText(CSV, data);
+            write(message);
         }
         return 0;
+    }
+
+    /// <summary>
+    /// Reads out all chirps in the csv file
+    /// </summary>
+    static private void read()
+    {
+        var input = File.ReadAllLines(CSV);
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            var line = Regex.Split(input[i], ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+            string userMessage = line[1].Replace("\"", "");
+            DateTime dateFormat = DateTime.UnixEpoch.AddSeconds(long.Parse(line[2]) + 7200);
+            string dateString = dateFormat.Month.ToString("D2") + "/" + dateFormat.Day.ToString("D2") + "/" +
+                               dateFormat.Year.ToString().Substring(2, 2) + " " + dateFormat.ToString("HH:mm:ss");
+
+            Console.WriteLine(line[0] + " @ " + dateString + ": " + userMessage);
+        }
+    }
+
+    /// <summary>
+    /// Writes the message to the csv file
+    /// Username is the currently logged in user
+    /// </summary>
+    /// <param name="message"> The message to write to the csv file </param>
+    static private void write(string message)
+    {
+        // Get metadata and cheep
+        string user = Environment.UserName;
+        long epoch = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        // Format and save
+        var data = user + ',' + '"' + message + '"' + ',' + epoch + '\n';
+        File.AppendAllText(CSV, data);
+
     }
 }
