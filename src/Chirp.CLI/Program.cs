9ac136b3c06b2ec9dd48942cc37d8ad87dae68fa
Matthias Nielsen <mscn@itu.dk>
2025-09-02 21:48:41 +0200

    Added database calls to program 2.b should be done
    CSVManager also deleted, but it's code is basicly copy-pasted into the database manager, so really smooth transtition here.
    
    Co-Authored-By: Dimitri Alessandro Nielsen <dini@itu.dk>
    Co-Authored-By: Maelkiz <63341740+Maelkiz@users.noreply.github.com>

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,13 +1,14 @@
 using Chirp.CLI;
 using Chirp.CLI.Models;
+using Microsoft.VisualBasic.CompilerServices;
+using SimpleDB;
 
-const string CSV = "../../assets/chirp_cli_db.csv";
+IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
 
-CsvManager csvm = new CsvManager(CSV);
-
-if (args[0] == "read")
-{
-    IEnumerable<Cheep> cheeps = csvm.GetCheeps();
+if (args[0] == "read") {
+    //Includes limit if one is given, else return all cheeps
+    var cheeps = args.Length > 1 ? database.Read(int.Parse(args[1])) : database.Read();
+    
     foreach (var cheep in cheeps)
     {
         // Read record and parse into desired format. Adds 2 hours in seconds to compensate for timezones.
@@ -18,5 +19,5 @@ else if (args[0] == "cheep")
 {
     // Get metadata and cheep, format and save
     Cheep cheep = new Cheep(Environment.UserName, args[1], DateTimeOffset.UtcNow.ToUnixTimeSeconds());
-    csvm.WriteCheep(cheep);
+    database.Store(cheep);
 }
