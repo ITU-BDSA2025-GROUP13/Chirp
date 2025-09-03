331a644bc356439ed8a1ef636bf9c53041e7091b
Matthias Nyman <natthias@proton.me>
2025-09-03 09:54:54 +0200

    use System.CommandLine for commandline parsing
    this introduces a dependency on https://www.nuget.org/packages/System.CommandLine
    
    Co-authored-by: Matthias Nielsen <matthias.s.nielsen@gmail.com>
    Co-authored-by: Lukas <lukasshaghashvili14@gmail.com>
    Co-authored-by: Dimitri Alessandro Nielsen <dini@itu.dk>
    Co-authored-by: Maelkiz <63341740+Maelkiz@users.noreply.github.com>

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,3 +1,4 @@
+using System.CommandLine;
 using Chirp.CLI.Models;
 using SimpleDB;
 
@@ -7,23 +8,62 @@ class Program
     {
         IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
 
-        if (args[0] == "read")
-        {
-            //Includes limit if one is given, else return all cheeps
-            var cheeps = args.Length > 1 ? database.Read(int.Parse(args[1])) : database.Read();
+        RootCommand rootCommand = new("Chirp: read or post cheeps");
 
-            foreach (var cheep in cheeps)
-            {
-                // Read record and parse into desired format. Adds 2 hours in seconds to compensate for timezones.
-                Console.WriteLine($"{cheep.Author} @ {DateTime.UnixEpoch.AddSeconds(cheep.Timestamp + 7200).ToString("MM/dd/yy HH:mm:ss").Replace("-", "/")}: {cheep.Message}");
-            }
-        }
-        else if (args[0] == "cheep")
+        Option<int> readOption = new("read")
         {
-            // Get metadata and cheep, format and save
-            Cheep cheep = new Cheep(Environment.UserName, args[1], DateTimeOffset.UtcNow.ToUnixTimeSeconds());
-            database.Store(cheep);
+            Description = "Read the number cheeps supplied on the commandline",
+        };
+        rootCommand.Options.Add(readOption);
+
+        Option<bool> readAllOption = new("read-all")
+        {
+            Description = "Read all the cheeps",
+        };
+        rootCommand.Options.Add(readAllOption);
+
+        Option<string> chirpOption = new("cheep")
+        {
+            Description = "Post a new cheep"
+        };
+        rootCommand.Options.Add(chirpOption);
+
+        rootCommand.SetAction(parseResult =>
+        {
+            if (parseResult.Errors.Count == 0)
+            {
+                int readCount = parseResult.GetValue(readOption);
+                if (readCount > 0)
+                {
+                    var cheeps = database.Read(readCount); // TODO: error on negative count
+                    readCheeps(cheeps);
+                }
+
+                if (parseResult.GetValue(readAllOption))
+                {
+                    var cheeps = database.Read();
+                    readCheeps(cheeps);
+                }
+
+                var message = parseResult.GetValue(chirpOption);
+                if (message is not null)
+                {
+                    Cheep cheep = new Cheep(Environment.UserName, message, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                    database.Store(cheep);
+                }
+            }
+        });
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
+    }
+
+    static void readCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (var cheep in cheeps)
+        {
+            Console.WriteLine($"{cheep.Author} @ {DateTime.UnixEpoch.AddSeconds(cheep.Timestamp + 7200).ToString("MM/dd/yy HH:mm:ss").Replace("-", "/")}: {cheep.Message}");
         }
-        return 0;
+
     }
 }
