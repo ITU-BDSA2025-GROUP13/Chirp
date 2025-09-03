35dd716876b9cc3fd8100394256f8a2eb4552ea7
Auto Formatter <formatter@example.com>
2025-09-03 08:18:27 +0000

    Fix formatting

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -5,23 +5,27 @@ using MissingFieldException = System.MissingFieldException;
 
 namespace SimpleDB;
 
-public sealed class CSVDatabase<T> : IDatabaseRepository<T> {
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
     private readonly string filepath = "../../assets/chirp_cli_db.csv";
-    
-    public IEnumerable<T> Read(int? limit = null) {
+
+    public IEnumerable<T> Read(int? limit = null)
+    {
         // Not using "using" keyword, as an IEnum is returned. Can be changed at some point.
         var csvReader = new CsvReader(new StreamReader(filepath), CultureInfo.InvariantCulture);
-        
+
         if (limit == null) return csvReader.GetRecords<T>();
-        
+
         var result = new List<T>();
-        for (int i = 0; i < limit && csvReader.Read(); i++) {
+        for (int i = 0; i < limit && csvReader.Read(); i++)
+        {
             result.Add(csvReader.GetRecord<T>());
         }
         return result;
     }
 
-    public void Store(T record) {
+    public void Store(T record)
+    {
         using var csvWriter = new CsvWriter(new StreamWriter(filepath, append: true), CultureInfo.InvariantCulture);
         csvWriter.WriteRecord(record);
         csvWriter.NextRecord();
--- src/SimpleDB/IDatabaseRepository.cs
+++ src/SimpleDB/IDatabaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace SimpleDB;
 
-public interface IDatabaseRepository<T> {
+public interface IDatabaseRepository<T>
+{
     public IEnumerable<T> Read(int? limit = null);
     public void Store(T record);
 }
