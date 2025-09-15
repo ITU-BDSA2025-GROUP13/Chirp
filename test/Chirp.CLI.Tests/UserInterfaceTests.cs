75c9d51fc834b41231899979bbc70f7cb241c4d4
Matthias Nyman <natthias@proton.me>
2025-09-15 14:14:37 +0200

    fix spelling of 'Author'

--- test/Chirp.CLI.Tests/UserInterfaceTests.cs
+++ test/Chirp.CLI.Tests/UserInterfaceTests.cs
@@ -8,9 +8,9 @@ namespace Chirp.CLI.Tests
         public void PrintCheeps_WritesExpectedOutputToConsole()
         {
             var testCheeps = new List<Cheep> {
-                new Cheep("Auther1", "This is the 1st test cheep.", 1690891760),
-                new Cheep("Auther2", "This is the 2nd test cheep.", 1690979858),
-                new Cheep("Auther3", "This is the 3rd test cheep.", 1690981487)
+                new Cheep("Author1", "This is the 1st test cheep.", 1690891760),
+                new Cheep("Author2", "This is the 2nd test cheep.", 1690979858),
+                new Cheep("Author3", "This is the 3rd test cheep.", 1690981487)
             };
 
             var originalOut = Console.Out;
@@ -22,9 +22,9 @@ namespace Chirp.CLI.Tests
                 UserInterface.PrintCheeps(testCheeps);
                 string actualOutput = testOut.ToString();
                 string expectedOutput =
-                    "Auther1 @ 01/08/23 14:09:20: This is the 1st test cheep." + Environment.NewLine +
-                    "Auther2 @ 02/08/23 14:37:38: This is the 2nd test cheep." + Environment.NewLine +
-                    "Auther3 @ 02/08/23 15:04:47: This is the 3rd test cheep." + Environment.NewLine;
+                    "Author1 @ 01/08/23 14:09:20: This is the 1st test cheep." + Environment.NewLine +
+                    "Author2 @ 02/08/23 14:37:38: This is the 2nd test cheep." + Environment.NewLine +
+                    "Author3 @ 02/08/23 15:04:47: This is the 3rd test cheep." + Environment.NewLine;
 
                 Assert.Equal(expectedOutput, actualOutput);
             }
