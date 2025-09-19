2cdb9dafe2eaa8e260f0e3de59c6724521c55c0f
Matthias Nielsen <mscn@itu.dk>
2025-09-19 12:23:35 +0200

    fix: Added error handling to endpoints

--- src/WebServer/Program.cs
+++ src/WebServer/Program.cs
@@ -6,13 +6,31 @@ IDatabaseRepository<Cheep> database = CSVDatabase<Cheep>.GetInstance();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/cheep/{message}", (String message) =>
+app.MapGet("/cheeps", () =>
 {
-    var cheep = new Cheep(Environment.UserName, message, DateTimeOffset.UtcNow.ToUnixTimeSeconds())
-    database.Store(cheep)
+    try
+    {
+        IEnumerable<Cheep> cheeps = database.Read();
+        return Results.Ok(cheeps);
+    }
+    catch (Exception e)
+    {
+        return Results.Problem(e.Message);
+    }
 });
 
-app.MapGet("/cheeps", () => database.Read());
-
+app.MapGet("/cheep/{message}", (String message) =>
+{
+    try
+    {
+        var cheep = new Cheep(Environment.UserName, message, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+        database.Store(cheep);
+        return Results.Ok($"Posted Cheep with message: {message} from {Environment.UserName}");
+    }
+    catch (Exception e)
+    {
+        return Results.BadRequest(e.Message);
+    }
+});
 
 app.Run();
