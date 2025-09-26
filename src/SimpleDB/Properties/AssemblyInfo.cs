7d2a2f88c3f85a49e0d8ce6e9eb935a8846691af
Matthias Nielsen <mscn@itu.dk>
2025-09-26 14:37:45 +0200

    Added E2E test for legacy branch
    - Already existed in WebServerTests, just moved it to more clearly show it's a E2E

--- src/SimpleDB/Properties/AssemblyInfo.cs
+++ src/SimpleDB/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Chirp.CSVDB.Tests")]
 [assembly: InternalsVisibleTo("WebServer.Tests")]
+[assembly: InternalsVisibleTo("Chirp.E2E.Tests")]
--- test/Chirp.E2E.Tests/E2ETests.cs
+++ test/Chirp.E2E.Tests/E2ETests.cs
@@ -3,36 +3,26 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using SimpleDB;
 
 
-public class WebServerTests
+public class E2ETests
 {
     private readonly HttpClient client;
-    private readonly CSVDatabase<Cheep> database;
     private readonly string sampleDB = "../../../assets/testSample.csv";
-
-    public WebServerTests()
+    
+    public E2ETests()
     {
         client = new WebApplicationFactory<WebServer.Program>().CreateClient();
     }
-
-
+    
     [Fact]
-    public async void readFromServer_ResultOK()
-    {
-        var task = client.GetAsync("/cheeps");
-        var response = await task;
-        Assert.True(response.IsSuccessStatusCode);
-    }
-
-
-    [Fact]
-    public async void writeToServer_ResultOK()
+    public async void writeAndRead_ResultOK()
     {
         string writeTestPath = "../../../assets/writeTest.csv";
         File.Copy(sampleDB, writeTestPath);
         CSVDatabase<Cheep>.SetPathForTest(writeTestPath);
 
-        var task = client.PostAsync("/cheep/tester/writeTest", null);
-        var response = await task;
+        var response = await client.PostAsync("/cheep/tester/writeAndReadTest", null);
+        Assert.True(response.IsSuccessStatusCode);
+        response = await client.GetAsync("/cheeps");
         Assert.True(response.IsSuccessStatusCode);
 
         File.Delete(writeTestPath);
