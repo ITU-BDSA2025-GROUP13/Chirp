07de5971fb09c269238b5856c9102a35b4483951
Maelkiz <krub@itu.dk>
2025-09-29 12:16:23 +0200

    fix: make shell script use absolute paths

--- scripts/initDB.sh
+++ scripts/initDB.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-sqlite3 /tmp/chirp.db < ../data/schema.sql
-sqlite3 /tmp/chirp.db < ../data/dump.sql
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+sqlite3 /tmp/chirp.db < "$SCRIPT_DIR/../data/schema.sql"
+sqlite3 /tmp/chirp.db < "$SCRIPT_DIR/../data/dump.sql"
