fd9e478a291e50aba63548b1ff485e6967f178f2
Lukas <lukasshaghashvili14@gmail.com>
2025-10-03 14:09:29 +0200

    fix: add repository and db to razor app

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,24 +1,26 @@
-using Chirp.SQLite;
 using Chirp.Models;
+using Chirp.Repository;
+using Chirp.DataBase;
 public interface ICheepService
 {
-    public List<CheepViewModel> GetCheeps(int pagenum = 0);
-    public List<CheepViewModel> GetCheepsFromAuthor(string author, int pagenum);
+    public List<Cheep> GetCheeps(int pagenum = 0);
+    public List<Cheep> GetCheepsFromAuthor(string author, int pagenum);
 }
 
 public class CheepService : ICheepService
 {
-    private static readonly IChirpFacade _db = new DBFacade();
+
+    private static readonly ICheepRepository _cheepRepository = new CheepRepository(new DB());
 
     /// Returns at most N cheeps from public timeline
-    public List<CheepViewModel> GetCheeps(int pagenum)
+    public List<Cheep> GetCheeps(int pagenum)
     {
-        return _db.ReadPage(pagenum).ToList();
+        return _cheepRepository.ReadPage(pagenum).ToList();
     }
 
     /// Returns at most N Cheeps from author
-    public List<CheepViewModel> GetCheepsFromAuthor(string author, int pagenum)
+    public List<Cheep> GetCheepsFromAuthor(string author, int pagenum)
     {
-        return _db.ReadPageFromAuthor(author, pagenum).ToList();
+        return _cheepRepository.ReadPageFromAuthor(author, pagenum).ToList();
     }
 }
