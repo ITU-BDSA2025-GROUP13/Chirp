697d2c20c10967606d4ec56d7cfa7c7ff81b0585
Lukas <lukasshaghashvili14@gmail.com>
2025-10-20 20:37:31 +0200

    test: add intgration and E2E test

---
+++ test/Chirp.Integration.Tests/E2E/EndToEndTests.cs
@@ -0,0 +1,55 @@
+﻿using Chirp.Razor;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Chirp.Infrastructure.Tests.Repository;
+
+public class EndToEndTests
+{
+    private WebApplicationFactory<Program>? _factory;
+
+    private void populateTable()
+    {
+        // this runs the program as is, it doesnt use an in memory database as we need program options to make it so
+        _factory = new WebApplicationFactory<Program>();
+    }
+
+    [Fact]
+    public async Task Get_PublicTimeline_ReturnsSuccessAndContainsExpectedContent()
+    {
+        populateTable();
+
+        if (_factory is null)
+        {
+            throw new InvalidOperationException("HttpClientFactory is not available.");
+        }
+
+        HttpClient client = _factory.CreateClient();
+        var response = await client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var html = await response.Content.ReadAsStringAsync();
+        html.Should().Contain("Public Timeline");
+        html.Should().Contain("Starbuck now is what we hear the worst."); // this is in the DBinit
+        html.Should().Contain("<a href=\"/Jacqualine Gilcoine\">");
+    }
+
+    [Fact]
+    public async Task Get_UserTimeline_ReturnsSuccessAndContainsExpectedContent()
+    {
+        populateTable();
+
+        if (_factory is null)
+        {
+            throw new InvalidOperationException("HttpClientFactory is not available.");
+        }
+
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/Jacqualine Gilcoine");
+        response.EnsureSuccessStatusCode();
+        var html = await response.Content.ReadAsStringAsync();
+        html.Should().Contain("Jacqualine Gilcoine's Timeline");
+        html.Should().Contain("Starbuck now is what we hear the worst."); // this is in the DBinit
+        html.Should().Contain("<a href=\"/Jacqualine Gilcoine\">");
+    }
+}
---
+++ test/Chirp.Integration.Tests/Integration/IntegrationTests.cs
@@ -0,0 +1,190 @@
+﻿using Chirp.Razor.Pages;
+using Chirp.Razor;
+using Chirp.Domain;
+using Chirp.Infrastructure;
+using FluentAssertions;
+using Moq;
+using MockQueryable.Moq;
+
+namespace Chirp.Integration.Tests;
+
+public class IntegrationTests
+{
+    private ICheepRepository? _cheepRepository;
+    private ICheepService? _cheepService;
+
+    string text6 = "I love Fortnite";
+    string text5 = "new 5";
+    string text4 = "new 4";
+    string text3 = "Newest cheep";
+    string text2 = "Older cheep";
+    string text1 = "Oldest cheep";
+
+    string name1 = "TestUser1";
+    string name2 = "TestUser2";
+    string name3 = "Karl Fortnite";
+    List<Cheep>? cheeps;
+
+    private void IntegrationTestsServiceAndRepo()
+    {
+        var author = new Author { AuthorId = 1, Name = name1, Email = "test@test.com" };
+        var authors = new List<Author>
+        {
+            author
+        };
+        cheeps = new List<Cheep>
+        {
+            new Cheep{ CheepId = 3, AuthorId = 1, Author = author, Text = text3, TimeStamp = DateTime.Now },
+            new Cheep{ CheepId = 2, AuthorId = 1, Author = author, Text = text2, TimeStamp = DateTime.Now.AddHours(-1) },
+            new Cheep{ CheepId = 1, AuthorId = 1, Author = author, Text = text1, TimeStamp = DateTime.Now.AddHours(-2) }
+        };
+        var mockContext = new Mock<IChirpDbContext>();
+        var mockCheepSet = cheeps.BuildMockDbSet();
+        var mockAuthorSet = authors.BuildMockDbSet();
+        mockContext.Setup(c => c.Authors).Returns(mockAuthorSet.Object);
+        mockContext.Setup(c => c.Cheeps).Returns(mockCheepSet.Object);
+        _cheepRepository = new CheepRepository(mockContext.Object);
+        _cheepService = new CheepService(_cheepRepository);
+    }
+
+    [Fact]
+    public void GetCheeps_FromMockedRepository_ReturnsAllCheepsInDescendingOrder()
+    {
+        IntegrationTestsServiceAndRepo();
+        if (_cheepService is null)
+        {
+            throw new InvalidOperationException("cheep service is not available.");
+        }
+        if (cheeps is null)
+        {
+            throw new InvalidOperationException("cheeps is not available.");
+        }
+
+        var result = _cheepService.GetCheeps(0);
+        result.Should().HaveCount(cheeps.Count);
+        result.First().Text.Should().Be(text3);
+        result.Last().Text.Should().Be(text1);
+    }
+
+    [Fact]
+    public void GetCheepsFromAuthor_FromMockedRepository_ReturnsAuthorsCheepsInDescendingOrder()
+    {
+        IntegrationTestsServiceAndRepo();
+        if (_cheepService is null)
+        {
+            throw new InvalidOperationException("cheep service is not available.");
+        }
+        var result = _cheepService.GetCheepsFromAuthor(name1);
+        result.Should().HaveCount(3);
+        result.First().Text.Should().Be(text3);
+        result.Last().Text.Should().Be(text1);
+    }
+
+    private void IntegrationTestsUIAndService()
+    {
+        var author1 = new Author { AuthorId = 1, Name = name1, Email = "test1@test.com" };
+        var author2 = new Author { AuthorId = 2, Name = name2, Email = "test2@test.com" };
+        var karlFortnite = new Author { AuthorId = 3, Name = name3, Email = "karl@fortnite.com" };
+        var authors = new List<Author> { author1, author2, karlFortnite };
+
+        var cheeps = new Stack<Cheep>();
+        cheeps.Push(new Cheep { CheepId = 1, AuthorId = 2, Author = author2, Text = text1, TimeStamp = DateTime.Now.AddHours(-5) });
+        cheeps.Push(new Cheep { CheepId = 2, AuthorId = 2, Author = author2, Text = text2, TimeStamp = DateTime.Now.AddHours(-4) });
+        cheeps.Push(new Cheep { CheepId = 3, AuthorId = 2, Author = author2, Text = text3, TimeStamp = DateTime.Now.AddHours(-3) });
+        cheeps.Push(new Cheep { CheepId = 4, AuthorId = 1, Author = author1, Text = text4, TimeStamp = DateTime.Now.AddHours(-2) });
+        cheeps.Push(new Cheep { CheepId = 5, AuthorId = 1, Author = author1, Text = text5, TimeStamp = DateTime.Now.AddHours(-1) });
+
+        var stackToList = () => cheeps.ToList();
+
+        var author2_autour = new Author
+        {
+            AuthorId = author2.AuthorId,
+            Name = author2.Name,
+            Email = author2.Email,
+            Cheeps = stackToList()
+        };
+
+        var karlFortniteWithCheeps = new Author
+        {
+            AuthorId = karlFortnite.AuthorId,
+            Name = karlFortnite.Name,
+            Email = karlFortnite.Email,
+            Cheeps = new List<Cheep>
+        {
+            new Cheep { CheepId = 100, AuthorId = 3, Author = karlFortnite, Text = text6, TimeStamp = DateTime.Now }
+        }
+        };
+
+        var mockRepository = new Mock<ICheepRepository>();
+
+        mockRepository
+            .Setup(c => c.GetAuthorFromAuthorID(1))
+            .Returns(author1);
+        mockRepository
+            .Setup(c => c.GetAuthorFromAuthorID(2))
+            .Returns(author2_autour);
+        mockRepository
+            .Setup(c => c.GetAuthorFromAuthorID(3))
+            .Returns(karlFortniteWithCheeps);
+
+        mockRepository
+            .Setup(c => c.PostAsync(It.IsAny<Cheep>()))
+            .Callback<Cheep>(c => cheeps.Push(c))
+            .Returns(Task.CompletedTask);
+
+        mockRepository
+            .Setup(c => c.ReadPageAsync(It.IsAny<int>()))
+            .ReturnsAsync(() => stackToList());
+
+        mockRepository
+            .Setup(c => c.ReadPageFromAuthorAsync(name3, It.IsAny<int>()))
+            .ReturnsAsync(karlFortniteWithCheeps);
+
+        _cheepService = new CheepService(mockRepository.Object);
+    }
+
+    [Fact]
+    public void UserTimelineModelOnGet_WithMockedService_PopulatesCheepsForSpecificUser()
+    {
+        IntegrationTestsUIAndService();
+        if (_cheepService is null)
+        {
+            throw new InvalidOperationException("cheep service is not available.");
+        }
+        var pageModel = new UserTimelineModel(_cheepService);
+        pageModel.OnGet(name3, 0);
+        pageModel.Cheeps.Should().NotBeNull();
+        pageModel.Cheeps.Should().HaveCount(1);
+        pageModel.Cheeps[0].AuthorName.Should().Be(name3);
+        pageModel.Cheeps[0].Text.Should().Be(text6);
+    }
+
+    [Fact]
+    public void PublicModelOnGet_AfterPostingCheep_ShowsNewCheepFirst()
+    {
+        string text = "New test cheep";
+        IntegrationTestsUIAndService();
+        if (_cheepService is null)
+        {
+            throw new InvalidOperationException("cheep service is not available.");
+        }
+
+        var pageModel = new PublicModel(_cheepService);
+
+        pageModel.OnGet(0);
+        var oldLength = pageModel.Cheeps.Count;
+        var oldFirstCheep = pageModel.Cheeps.FirstOrDefault();
+
+        _cheepService.PostCheep(text, 1);
+
+        pageModel.OnGet(0);
+        var newLength = pageModel.Cheeps.Count;
+        var newFirstCheep = pageModel.Cheeps.First();
+
+        pageModel.Cheeps.Should().NotBeNull();
+        newLength.Should().Be(oldLength + 1);
+        newFirstCheep.AuthorName.Should().Be(name1);
+        newFirstCheep.Text.Should().Be(text);
+        newFirstCheep.Text.Should().NotBe(oldFirstCheep?.Text);
+    }
+}
