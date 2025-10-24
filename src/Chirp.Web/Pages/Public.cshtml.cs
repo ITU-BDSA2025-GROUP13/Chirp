1a102e7a100e760393659a3a061b3970bd060e5e
Maelkiz <krub@itu.dk>
2025-10-24 13:12:58 +0200

    feat: added pagination buttons
    s

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Chirp.Infrastructure.Services;
+using Chirp.Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,6 +8,9 @@ public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepDTO> Cheeps { get; set; }
+    public int CurrentPage { get; set; }
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage => CurrentPage > 0;
 
     public PublicModel(ICheepService service)
     {
@@ -17,7 +20,9 @@ public class PublicModel : PageModel
 
     public ActionResult OnGet([FromQuery] int page)
     {
+        CurrentPage = page;
         Cheeps = _service.GetMainPageCheeps(page);
+        HasNextPage = _service.GetMainPageCheeps(page + 1).Any();
         return Page();
     }
 }
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Chirp.Infrastructure.Services;
+using Chirp.Infrastructure.Services;
+using System.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,6 +9,9 @@ public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepDTO> Cheeps { get; set; }
+    public int CurrentPage { get; set; }
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage => CurrentPage > 0;
 
     public UserTimelineModel(ICheepService service)
     {
@@ -17,7 +21,9 @@ public class UserTimelineModel : PageModel
 
     public ActionResult OnGet(string author, [FromQuery] int page)
     {
+        CurrentPage = page;
         Cheeps = _service.GetCheepsFromAuthorName(author, page);
+        HasNextPage = _service.GetCheepsFromAuthorName(author, page + 1).Any();
         return Page();
     }
 }
