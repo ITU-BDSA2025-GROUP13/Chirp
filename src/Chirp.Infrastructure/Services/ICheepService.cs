f8885e709a8a181cfeba01217ec6c6261aa8573f
Maelkiz <karl.ruby2005@gmail.com>
2025-11-10 01:02:57 +0100

    feat: implement delete button in the ui

--- src/Chirp.Infrastructure/Services/ICheepService.cs
+++ src/Chirp.Infrastructure/Services/ICheepService.cs
@@ -3,6 +3,11 @@ namespace Chirp.Infrastructure.Services
 {
     public interface ICheepService
     {
+        /// <summary>
+        /// Returns at the specified page of cheepDTOs.
+        /// </summary>
+        /// <param name="pagenum"></param>
+        /// <returns></returns>
         public List<CheepDTO> GetMainPageCheeps(int pagenum = 0);
         /// <summary>
         /// Returns at most N Cheeps from author. Will create a new author on request if no author exist.
@@ -11,10 +16,11 @@ namespace Chirp.Infrastructure.Services
         /// <param name="pagenum"></param>
         /// <returns>The cheeps associated with an author, or a emptyList if author doesn't exist</returns>
         public List<CheepDTO> GetCheepsFromAuthorName(string authorName, int pagenum = 0);
+        
         public List<CheepDTO> GetCheepsFromAuthorID(string authorID, int pagenum = 0);
         public List<CheepDTO> GetCheepsFromAuthorEmail(string authorEmail, int pagenum = 0);
         public string GetAuthorIDFromName(string authorName);
-        public void PostCheep(string text, int authorID);
+        public void PostCheep(string text, string authorID);
         public void DeleteCheep(int cheepID);
     }
 }
