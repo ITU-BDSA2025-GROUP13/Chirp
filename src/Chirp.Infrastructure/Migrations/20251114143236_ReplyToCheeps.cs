c4a94072215ecef4f83cfa1de61e25e1948abb83
Matthias Nielsen <mscn@itu.dk>
2025-11-14 15:41:03 +0100

    mangler at fikse usertimeline og tests

---
+++ src/Chirp.Infrastructure/Migrations/20251114143236_ReplyToCheeps.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplyToCheeps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCheepCheepId",
+                table: "message",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_message_ParentCheepCheepId",
+                table: "message",
+                column: "ParentCheepCheepId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_message_message_ParentCheepCheepId",
+                table: "message",
+                column: "ParentCheepCheepId",
+                principalTable: "message",
+                principalColumn: "message_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_message_message_ParentCheepCheepId",
+                table: "message");
+
+            migrationBuilder.DropIndex(
+                name: "IX_message_ParentCheepCheepId",
+                table: "message");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCheepCheepId",
+                table: "message");
+        }
+    }
+}
