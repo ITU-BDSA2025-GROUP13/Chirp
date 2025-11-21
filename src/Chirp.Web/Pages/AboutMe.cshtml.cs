8a22f42fe5e7e0eba2e30de9dcb9374620d74553
Maelkiz <krub@itu.dk>
2025-11-21 13:29:25 +0100

    feat: add about me page for GDPR

---
+++ src/Chirp.Web/Pages/AboutMe.cshtml.cs
@@ -0,0 +1,14 @@
+using Chirp.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
