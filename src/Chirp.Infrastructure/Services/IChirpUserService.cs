e8cc56a813523a540319e884dee73f014d747ee3
Maelkiz <krub@itu.dk>
2025-11-23 20:14:24 +0100

    feat: make about me page display followed users

--- src/Chirp.Infrastructure/Services/IChirpUserService.cs
+++ src/Chirp.Infrastructure/Services/IChirpUserService.cs
@@ -3,4 +3,5 @@ namespace Chirp.Infrastructure.Services;
 public interface IChirpUserService
 {
     public void ToggleUserFollowing(string userAName, string userBName);
+    public List<string> GetFollowedUsernames(string username);
 }
